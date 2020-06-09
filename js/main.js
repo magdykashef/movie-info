a3ca01c15261960ebf33b64be8808e7bb5466ba2
magdykashef <magdy7753@gmail.com>
2020-06-09 21:03:54 +0200

    small fixes

--- js/main.js
+++ js/main.js
@@ -1,35 +1,56 @@
+// inner
 $(document).ready(() => {
   $('#searchForm').on('submit', (e) => {
     e.preventDefault();
     let searchText = $('#searchText').val();
     getMovies(searchText);
   });
+  getMovie();
 });
 
+function getApiKey(){
+  if(sessionStorage.getItem('api') && ($('#apiKey').val().length == 0)){
+    let api = sessionStorage.getItem('api')
+    $('#apiKey').attr("placeholder", api);
+    return api;
+  }
+  else if(! (sessionStorage.getItem('api') ) && ($('#apiKey').val().length == 0)){
+    console.log("You Have to Enter API KEY");
+  }
+  else{
+    let apiValue = $('#apiKey').val();
+    sessionStorage.setItem('api', apiValue);
+    let api = sessionStorage.getItem('api');
+    $('#apiKey').attr("placeholder", api);
+    return api;
+    }
+  return api;  
+  }
+
 function getMovies(searchText) {
-  //'http://www.omdbapi.com/?s=' + searchText + '&apikey=8b892b12'
-  axios.get('https://www.imdb.com/find?q=endgame&ref_=nv_sr_sm')
-    .then((response) => {
-      console.log(response);
-      // let movies = response.data.Search;
-      // let output = '';
-      // $.each(movies, (index, movie) => {
-      //   output += `
-      //     <div class="col-md-3">
-      //       <div class="well text-center">
-      //         <img src="${movie.Poster}">
-      //         <h5>${movie.Title}</h5>
-      //         <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
-      //       </div>
-      //     </div>
-      //   `;
-      // });
-      // $('#movies').html(output);
-    })
-    .catch((err) => {
-      console.log(err);
+  axios.get('http://www.omdbapi.com/?s='+ searchText +'&apikey='+ getApiKey())
+  .then((response) => {
+    console.log(response);
+    let movies = response.data.Search;
+    let output = '';
+    $.each(movies, (index, movie) => {
+      output += `
+        <div class="col-md-3">
+          <div class="well text-center">
+            <img src="${movie.Poster}">
+            <h5>${movie.Title}</h5>
+            <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
+          </div>
+        </div>
+      `;
     });
+    $('#movies').html(output);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 }
+
 
 function movieSelected(id) {
   sessionStorage.setItem('movieId', id);
@@ -39,8 +60,10 @@ function movieSelected(id) {
 
 function getMovie() {
   let movieId = sessionStorage.getItem('movieId');
-  //'http://www.omdbapi.com/?i='+ movieId + '&apikey=8b892b12'
-  axios.get('https://www.imdb.com/find?q=endgame&ref_=nv_sr_sm')
+  let sessionApi = sessionStorage.getItem('api');
+  $('#apiKey').attr("placeholder", sessionApi);
+  //api='1b234b56';
+  axios.get('http://www.omdbapi.com/?i='+ movieId + '&apikey='+ sessionApi)
     .then((response) => {
       console.log(response);
       let movie = response.data;
